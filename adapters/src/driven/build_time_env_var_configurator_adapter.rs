ed1644f761872d234c1dd94a3f0422adf1e64f20
gsenden <ger@senden.dev>
2025-11-29 17:21:59 +0100

    #1 Fixed configuration adapter to reflect the changes in making the domain hold the data

--- adapters/src/driven/build_time_env_var_configurator_adapter.rs
+++ adapters/src/driven/build_time_env_var_configurator_adapter.rs
@@ -1,99 +1,69 @@
+// adapters/src/driven/build_time_env_var_configurator_adapter.rs
+use engine::domain::configuration::Configuration;
 use engine::domain::errors::ConfigurationError;
-use oauth2::{AuthUrl, ClientId, ClientSecret, RedirectUrl, TokenUrl};
-use engine::ports::driven::configurator_driven_port::{ConfiguratorFactoryPort, ConfiguratorPort};
 use engine::domain::default_values::configurator_defaults::*;
+use engine::ports::driven::configurator_driven_port::ConfiguratorPort;
+use oauth2::{AuthUrl, ClientId, RedirectUrl, TokenUrl};
 
-#[derive(Debug)]
-pub struct BuildTimeEnvVarConfigurator {
-    auth_url: AuthUrl,
-    token_url: TokenUrl,
-    client_id: ClientId,
-    redirect_url: RedirectUrl,
-}
+pub struct BuildTimeEnvVarConfiguratorPort;
 
-impl ConfiguratorPort for BuildTimeEnvVarConfigurator {
-    fn auth_url(&self) -> &AuthUrl {
-        &self.auth_url
-    }
+impl ConfiguratorPort for BuildTimeEnvVarConfiguratorPort {
+    fn load(&self) -> Result<Configuration, ConfigurationError> {
+        let auth_url = option_env!("AUTH_URL")
+            .unwrap_or(DEFAULT_AUTH_URL)
+            .to_string();
+        let token_url = option_env!("TOKEN_URL")
+            .unwrap_or(DEFAULT_TOKEN_URL)
+            .to_string();
+        let client_id = option_env!("CLIENT_ID")
+            .unwrap_or(DEFAULT_CLIENT_ID)
+            .to_string();
+        let redirect_url = option_env!("REDIRECT_URL")
+            .unwrap_or(DEFAULT_REDIRECT_URL)
+            .to_string();
 
-    fn token_url(&self) -> &TokenUrl {
-        &self.token_url
-    }
-
-    fn client_id(&self) -> &ClientId {
-        &self.client_id
-    }
-
-    fn redirect_url(&self) -> &RedirectUrl {
-        &self.redirect_url
-    }
-}
-
-pub struct BuildTimeEnvVarConfiguratorFactory;
-impl ConfiguratorFactoryPort for BuildTimeEnvVarConfiguratorFactory {
-    type Port = BuildTimeEnvVarConfigurator;
-
-    fn load(&self) -> Result<Self::Port, ConfigurationError>
-    where
-        Self: Sized
-    {
-        let auth_url = option_env!("AUTH_URL").unwrap_or(DEFAULT_AUTH_URL).to_string();
-        let token_url = option_env!("TOKEN_URL").unwrap_or(DEFAULT_TOKEN_URL).to_string();
-        let client_id = option_env!("CLIENT_ID").unwrap_or(DEFAULT_CLIENT_ID).to_string();
-        let redirect_url = option_env!("REDIRECT_URL").unwrap_or(DEFAULT_REDIRECT_URL).to_string();
-
-        Ok(BuildTimeEnvVarConfigurator {
+        Ok(Configuration {
             auth_url: AuthUrl::new(auth_url)?,
             token_url: TokenUrl::new(token_url)?,
             client_id: ClientId::new(client_id),
-            redirect_url: RedirectUrl::new(redirect_url)?
+            redirect_url: RedirectUrl::new(redirect_url)?,
         })
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use engine::ports::driven::configurator_driven_port::{ConfiguratorFactoryPort, ConfiguratorPort};
-    use crate::driven::build_time_env_var_configurator_adapter::*;
+    use super::*;
 
     #[test]
     fn auth_url_from_env() {
-        // When the environment variables are loaded
-        let factory = BuildTimeEnvVarConfiguratorFactory;
-        let environment_variables = factory.load().unwrap();
+        let port = BuildTimeEnvVarConfiguratorPort;
+        let config = port.load().unwrap();
 
-        // Then the client id is loaded from the env file
-        assert_eq!(environment_variables.auth_url.to_string(), DEFAULT_AUTH_URL);
+        assert_eq!(config.auth_url.to_string(), DEFAULT_AUTH_URL);
     }
 
     #[test]
     fn client_id_from_env() {
-        // When the environment variables are loaded
-        let factory = BuildTimeEnvVarConfiguratorFactory;
-        let environment_variables = factory.load().unwrap();
+        let port = BuildTimeEnvVarConfiguratorPort;
+        let config = port.load().unwrap();
 
-        // Then the client id is loaded from the env file
-        assert_eq!(environment_variables.client_id().to_string(), DEFAULT_CLIENT_ID);
+        assert_eq!(config.client_id.as_str(), DEFAULT_CLIENT_ID);
     }
 
     #[test]
     fn token_url_from_env() {
-        // When the environment variables are loaded
-        let factory = BuildTimeEnvVarConfiguratorFactory;
-        let environment_variables = factory.load().unwrap();
+        let port = BuildTimeEnvVarConfiguratorPort;
+        let config = port.load().unwrap();
 
-        // Then the token url loaded from the env file
-        assert_eq!(environment_variables.token_url().to_string(), DEFAULT_TOKEN_URL);
+        assert_eq!(config.token_url.to_string(), DEFAULT_TOKEN_URL);
     }
 
     #[test]
     fn redirect_url_from_env() {
-        // When the environment variables are loaded
-        let factory = BuildTimeEnvVarConfiguratorFactory;
-        let environment_variables = factory.load().unwrap();
+        let port = BuildTimeEnvVarConfiguratorPort;
+        let config = port.load().unwrap();
 
-        // Then the token url loaded from the env file
-        assert_eq!(environment_variables.redirect_url().as_str(), DEFAULT_REDIRECT_URL);
+        assert_eq!(config.redirect_url.as_str(), DEFAULT_REDIRECT_URL);
     }
-
 }
