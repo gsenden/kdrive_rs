61220ffbbca106686f03561ea809de08882f8f72
gsenden <ger@senden.dev>
2025-11-29 17:17:46 +0100

    #1 Refactored/combined token files and configuration files

--- engine/src/domain/mod.rs
+++ engine/src/domain/mod.rs
@@ -1,9 +1,7 @@
 pub mod errors;
 pub mod callback_endpoint;
 pub mod test_helpers;
-pub mod configurator;
 pub mod engine;
-pub mod token_store;
 pub mod default_values;
 pub mod tokens;
 pub mod configuration;
