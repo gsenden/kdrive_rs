83599023b9b6164c97846869e77f7009a7c2cdab
Ger Senden <gsenden@outlook.com>
2025-12-21 00:15:31 +0100

    #9 feat: Created connecting screen.

--- clients/desktop/src/ui/views/connecting_view.rs
+++ clients/desktop/src/ui/views/connecting_view.rs
@@ -1,12 +1,28 @@
 use common::ports::i18n_driven_port::I18nDrivenPort;
 use dioxus::prelude::*;
 use common::domain::text_keys::TextKeys::ConnectingToServiceMessage;
+use crate::ui::components::TitleBanner;
 
 #[component]
 pub fn ConnectingView<I18nPort: I18nDrivenPort + 'static>(i18n: I18nPort) -> Element {
     rsx! {
-        div { class: "loading",
-            p { {i18n.t(ConnectingToServiceMessage)} }
+        div {
+            class: "min-h-screen flex flex-col items-center bg-[#0f1116] p-8 text-white",
+
+            div {
+                class: "pt-[25vh] flex flex-col items-center gap-y-10 w-full max-w-xl",
+                TitleBanner { i18n: i18n.clone() },
+            }
+
+            div {
+                class: "mt-8 flex flex-col items-center text-center max-w-xl",
+                h2 {
+                    class: "text-2xl loading",
+                    {i18n.t(ConnectingToServiceMessage)}
+                }
+
+            }
         }
+
     }
 }
