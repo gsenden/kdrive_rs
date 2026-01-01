f458367830b3043bfa941dc98531748da479f8ec
gsenden <ger@senden.dev>
2026-01-02 00:23:30 +0100

    Test

--- server/adapters/src/driven/kdrive_authenticator_adapter.rs
+++ server/adapters/src/driven/kdrive_authenticator_adapter.rs
@@ -52,6 +52,9 @@ impl AuthenticatorDrivenPort for KDriveAuthenticator {
         let (auth_url, csrf_token) = self.client
             .authorize_url(CsrfToken::new_random)
             .add_scope(Scope::new("openid".to_string()))
+            .add_scope(Scope::new("profile".to_string()))
+            .add_scope(Scope::new("email".to_string()))
+            .add_scope(Scope::new("phone".to_string()))
             .add_extra_param("access_type", "offline")
             .set_pkce_challenge(pkce_challenge)
             .url();
